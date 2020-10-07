48e334da1b454c2eae67fd624a63d38a1e4e633f
Yu-Rui0628 <54125563+Yu-Rui0628@users.noreply.github.com>
2020-10-07 17:47:38 +1100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React, {Component} from 'react';
-import './App.css';
-import NavBar from './components/NavBar';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
 import Banner from "./components/banner";
-
+import Pricetable from "./components/Pricetable";
+import Pricetable2 from "./components/Pricetable2";
 class App extends Component {
-	render(){
-		return(
-		    <React.Fragment>
-		    	<NavBar/>
-		    	<Banner/>
-		    </React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Banner />
+        <Pricetable />
+        <Pricetable2 />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
