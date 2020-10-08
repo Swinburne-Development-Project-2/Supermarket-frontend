7b104abfd443e3d8089c631218e66e7346a84c63
Long Nguyen <nguyenhailong253@gmail.com>
2020-10-08 14:55:54 +1100

    refactor search bar, implement axios

---
+++ src/components/search/SearchBar.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import styles from './SearchBar.module.css';
+import axios from "axios";
+
+class SearchBar extends Component {
+    state = {
+        keyword: '',
+    }
+
+    handleKeywordChange = event => {
+        const keyword = event.target.value;
+        this.setState({ keyword });
+    }
+
+    handleEnter = event => 
+        event.keyCode === 13 && this.handleSearch(event);
+
+    handleSearch = event => {
+        event.preventDefault();
+        console.log("test keyword search ", this.state.keyword);
+
+        axios.get(`http://localhost:3001/home/search/${this.state.keyword}`)
+            .then(response => this.props.handlePriceData(response));
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    id="keyword" 
+                    name="keyword" 
+                    className={styles.searchBox} 
+                    placeholder="i.e beef, pork, etc"
+                    onChange={this.handleKeywordChange}
+                    onKeyDown={this.handleEnter}
+                />
+                <button type="submit" className={styles.searchButton} onClick={this.handleSearch}>
+                    Search
+                </button>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
